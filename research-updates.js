c3f9ddc55fb1ddec9912bbb4e4ef64d4c5dd9dc9
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-06 11:52:56 +0330

    Bump roadmap sprint alignment assets

--- research-updates.js
+++ research-updates.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const version = "20260906-1";
+  const version = "20260906-2";
   document.write(`<script src="research-core.js?v=${version}"><\/script>`);
   document.write(`<script src="execution-updates.js?v=${version}"><\/script>`);
   document.write(`<script src="okr-sprint-alignment.js?v=${version}"><\/script>`);
