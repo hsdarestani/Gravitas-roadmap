86a99cc10f59366453660a85736ab7c0b3ed1917
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-06 11:49:51 +0330

    Load OKR alignment for the current sprint

--- research-updates.js
+++ research-updates.js
@@ -1,7 +1,8 @@
 (() => {
   "use strict";
 
-  const version = "20260803-9";
+  const version = "20260906-1";
   document.write(`<script src="research-core.js?v=${version}"><\/script>`);
   document.write(`<script src="execution-updates.js?v=${version}"><\/script>`);
+  document.write(`<script src="okr-sprint-alignment.js?v=${version}"><\/script>`);
 })();
